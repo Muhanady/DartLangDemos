2db6b5de3f594207ce23898e490fce1cb2e367b4
muhanady <mohi88@gmail.com>
2014-02-07 15:31:55 +0200

    750

--- web/js/main.js
+++ web/js/main.js
@@ -6,6 +6,6 @@ $(".animasyonoynat").click(function(){
                setTimeout(function() {
                        $(".Cover").fadeIn();
                        $("#tableDiv .MYXOAlert").addClass("Animated");
-               }, 2000);
+               }, 750);
        });
 })
